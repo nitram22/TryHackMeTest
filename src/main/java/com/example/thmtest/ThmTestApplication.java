f89812c91847461f699f3c0adbe810031b5f9aaa
Martin Wilhelm <martin.wilhelm@essendi.de>
2022-05-21 11:17:45 +0200

    Changes

---
+++ src/main/java/com/example/thmtest/ThmTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.thmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThmTestApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ThmTestApplication.class, args);
+	}
+
+}
